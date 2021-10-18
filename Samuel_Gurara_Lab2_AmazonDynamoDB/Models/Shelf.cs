02a2cf0a297a8a4bca75b8cd9ac180387afdfd92
Samuel <Samueljambo81@gmail.com>
2021-10-18 12:12:39 -0400

    Modification made on project Lab2

---
+++ Samuel_Gurara_Lab2_AmazonDynamoDB/Models/Shelf.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samuel_Gurara_Lab2_AmazonDynamoDB
+{
+    
+        [DynamoDBTable("Bookshelf")]
+        public class Shelf
+        {
+            [DynamoDBProperty("UserName")]
+            public string User { get; set; }
+
+            [DynamoDBProperty("Books")]
+            public Dictionary<string, int> Books { get; set; }
+        }
+}
